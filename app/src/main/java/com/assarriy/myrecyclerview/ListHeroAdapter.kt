61ce4ece0e56823b96f0bdcfd2c5e430c19332e8
assarriy <wibowoassariy23@gmail.com>
2023-11-10 21:05:30 +0700

    Update

--- app/src/main/java/com/assarriy/myrecyclerview/ListHeroAdapter.kt
+++ app/src/main/java/com/assarriy/myrecyclerview/ListHeroAdapter.kt
@@ -1,23 +1,34 @@
 package com.assarriy.myrecyclerview
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    }
+class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
+    RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        val view: View =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val (name, description, photo) = listHero[position]
+        holder.imgPhoto.setImageResource(photo)
+        holder.tvName.text = name
+        holder.tvDescription.text = description
     }
+
+    override fun getItemCount(): Int = listHero.size
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        val tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        val tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+    }
+
 }
